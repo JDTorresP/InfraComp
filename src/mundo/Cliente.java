e35275da8e5a3d7252edddde322c081febe2a72f
Joan Torres <jd.torres11@uniandes.edu.co>
2016-09-07 08:32:29 -0500

    Commit Inicial
    Se provee la estructura general del proyecto y el documento inicial.

---
+++ src/mundo/Cliente.java
@@ -0,0 +1,11 @@
+package mundo;
+
+public class Cliente extends Thread implements Comparable<Cliente>{
+
+	@Override
+	public int compareTo(Cliente o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
