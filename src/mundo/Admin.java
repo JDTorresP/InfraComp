143f1125b54abbff750898d5905f88a590b6bc44
JDTawa <jd.torres11@uniandes.edu.co>
2016-09-07 11:28:22 -0500

    Se cargan los archivos, se inicializa el buffer

--- src/mundo/Admin.java
+++ src/mundo/Admin.java
@@ -1,10 +1,63 @@
 package mundo;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Admin {
+	
+	private static final String NOMBRE_ARCHIVO = "config.properties";
+	private static final String UBI_ARCHIVO = "data/";
+	private int numClientes = 0;
+	private int numServidores = 0;
+	private int tamanoBuffer = 0;
+	
+	//-------------------
+	//-----Metodos-------
+	//-------------------
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/**
+	 * Metodo que se encarga de leer el archivo de configuracion con los parametros de inicializacion del buffer
+	 * Pos: Los parametros del archivo de configuracion inicializan el buffer y quedan almacenados
+	 * @throws IOException 
+	 */
+	
+	public Admin() throws IOException
+	{
+		Properties prop = new Properties();
+		File file = new File( UBI_ARCHIVO+ NOMBRE_ARCHIVO);
+		FileInputStream fileInput = new FileInputStream(file);
+		Properties properties = new Properties();
+		properties.load(fileInput);
 
+		numClientes = Integer.parseInt( properties.getProperty("numClientes") );
+		numServidores = Integer.parseInt( properties.getProperty("numServidores") );
+		tamanoBuffer = Integer.parseInt( properties.getProperty("tamanoBuffer") );
+		fileInput.close();
+		Buffer canal = new Buffer(numClientes, numServidores, tamanoBuffer);
+	}
+	public static void main(String[] args) throws IOException {
+		
+		Admin a = new Admin();
+	}
+	public int getNumClientes() {
+		return numClientes;
+	}
+	public void setNumClientes(int numClientes) {
+		this.numClientes = numClientes;
+	}
+	public int getNumServidores() {
+		return numServidores;
+	}
+	public void setNumServidores(int numServidores) {
+		this.numServidores = numServidores;
+	}
+	public int getTamanoBuffer() {
+		return tamanoBuffer;
+	}
+	public void setTamanoBuffer(int tamanoBuffer) {
+		this.tamanoBuffer = tamanoBuffer;
 	}
 
 }
--- src/mundo/Buffer.java
+++ src/mundo/Buffer.java
@@ -2,4 +2,33 @@ package mundo;
 
 public class Buffer {
 
+	private int numClientes = 0 ;
+	private int numServidores = 0;
+	private int tamanoBuffer = 0;
+	
+	
+	public Buffer(int numC,int numS,int tam)
+	{
+		setNumClientes(numC);
+		setNumServidores(numS);
+		setTamanoBuffer(tam);
+	}
+	public int getNumClientes() {
+		return numClientes;
+	}
+	public void setNumClientes(int numClientes) {
+		this.numClientes = numClientes;
+	}
+	public int getNumServidores() {
+		return numServidores;
+	}
+	public void setNumServidores(int numServidores) {
+		this.numServidores = numServidores;
+	}
+	public int getTamanoBuffer() {
+		return tamanoBuffer;
+	}
+	public void setTamanoBuffer(int tamanoBuffer) {
+		this.tamanoBuffer = tamanoBuffer;
+	}
 }
